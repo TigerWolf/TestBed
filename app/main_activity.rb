470ddcc28121121a904e5f20bc8059fa776a69da
TigerWolf <hiddentiger@gmail.com>
2014-10-26 00:31:27 +1030

    add new functions to examples

--- app/main_activity.rb
+++ app/main_activity.rb
@@ -5,11 +5,25 @@ class MainActivity < Android::App::Activity
     begin
       puts "started - testbed"
       c = Nexusmotion::Client.new
-      url = "https://posttestserver.com/post.php?dir=nexusmotion"
+      # url = "https://posttestserver.com/post.php?dir=nexusmotion"
+      url = "http://httpbin.org/post"
       data = [["id", 33443]]
       result = c.post(url, data)
+      puts "Success?:"
+      puts result.success?
       puts "Result:"
-      puts result
+      puts result.body
+      puts "Code:"
+      puts result.code
+      url = "http://httpbin.org/get"
+      result = c.get(url)
+      puts "Success?:"
+      puts result.success?
+      puts "Result:"
+      puts result.body
+      puts "Code:"
+      puts result.code
+
     rescue Exception => e
       p "An error occurred: #{e.inspect}"
     end
